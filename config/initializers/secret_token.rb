219ed7a064f2f92299b299e1381e888e7ff746b0
Renato Karvalho <renato@karvalho.com.br>
2013-07-26 15:19:40 -0300

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Decaprails::Application.config.secret_key_base = 'de48c691a9aeacec04e05defb400656c388c7d75b77babb1a13ea0acc39088c06f7c2781bed173c6a61883a2609d851e7c85e463d7f32b19b29babf8e8f19995'
